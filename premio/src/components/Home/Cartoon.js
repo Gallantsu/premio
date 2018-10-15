06b22b3d168488a66b27792d74f70b66b79a114e
Gallantsu <suyong0503@163.com>
2018-10-15 18:26:38 +0800

    修改cartoon

--- premio/src/components/Home/Cartoon.js
+++ premio/src/components/Home/Cartoon.js
@@ -3,15 +3,14 @@
  * Time:2018.10.15
  * Mail:604747817@qq.com
  */
-class Cartoon {
+export class Cartoon {
 	constructor(options){
 		this.defaults={
 			cartoonWidth:1200,
 			cartoonHeight:800,
-			autoPlay:true,
 			playInterval:5000,
 			playType:'random',
-			isControlled:false,
+			isControlled:false, //是否有按钮控制页面动画切换
 			prevBtn:null,
 			nextBtn:null
 		}
@@ -22,16 +21,45 @@ class Cartoon {
 	init(){
 		this.cartoonItems = $(this).find("li");
 		this.cartoonLen = this.cartoonItems.length;
-		
+		this.currentItem = 0;
+		if(this.isControlled){
+			if(!prevBtn){
+				throw new Error("Please sure the prev button"); 
+				return;
+			}
+			if(!nextBtn){
+				throw new Error("Please sure the next button"); 
+				return;
+			}
+
+			$(this.prevBtn).bind("click",function(){
+				
+			});
+
+			$(this.nextBtn).bind("click",function(){
+				
+			});
+
+			this.play();
+		}else{
+			this.play();
+		}
+	}
+
+	play(step){
+		if(!step){
+				
+		}
 	}
 
 	nextPlay(){
-		
+
 	}
 
 	prevPlay(){
-		
+
 	}
 
 
 }
+
